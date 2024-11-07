43a7f2c8338d68950540c3c4d58141a507d6b84c
Prakash Shanbhag <prakash@signalsiq.ai>
2024-11-07 13:33:43 +0530

    Add stars to indicate quality

--- generate_summary.py
+++ generate_summary.py
@@ -1,8 +1,32 @@
 # Description: This script reads the CPU and CUDA execution times from the log files and generates a
 # markdown file with the combined execution times and sample audio links.
+import json
 import pandas as pd
 from pathlib import Path
 
+
+class Config:
+    def __init__(self, json_file):
+        # Load the JSON data into a dictionary for easy access
+        with open(json_file, "r") as f:
+            self.data = json.load(f)
+
+    def get_info(self, vocoder, model, info_type):
+        """
+        Fetches the requested info (either 'stars' or 'link') for the given vocoder and model.
+
+        :param model: The model name as a string
+        :param vocoder: The vocoder name as a string
+        :param info_type: The type of information to fetch ('stars' or 'link')
+        :return: The requested info if found, otherwise None
+        """
+        for entry in self.data:
+            if entry["vocoder"] == vocoder and entry["model"] == model:
+                return entry.get(info_type, None)
+        return None
+
+
+config = Config('config.json')
 # Load CPU and CUDA log files into DataFrames
 cpu_df = pd.read_csv('cpu.execution_times.log', sep='|', skipinitialspace=True, usecols=[
                      1, 2, 3], names=["Model", "Vocoder", "Duration"])
@@ -38,12 +62,25 @@ def extract_last_two_parts(path):
 def get_audio_path(model, vocoder):
     src = 'https://github.com/praks-1529/poly-phonic/raw/refs/heads/main/samples/speech_%s_%s_output.wav' % (
         extract_last_two_parts(model), extract_last_two_parts(vocoder))
-    return f'[Play sample](https://gabalpha.github.io/read-audio/?p={src})'
+    return f'[Play simple]({config.get_info(vocoder, model, "link")})'
+
+
+def get_stars(model, vocoder):
+    stars_to_str = {
+        -1: '☆☆☆☆',
+        0: '☆☆☆☆',
+        1: '⭐☆☆☆',
+        2: '⭐⭐☆☆',
+        3: '⭐⭐⭐☆',
+        4: '⭐⭐⭐⭐',
+    }
+    return stars_to_str[config.get_info(vocoder, model, "stars")]
 
 
 # Prepare the final DataFrame with the desired columns and formatting
 final_df = pd.DataFrame({
     'Sample': merged_df.apply(lambda x: get_audio_path(x['Model'], x['Vocoder']), axis=1),
+    'Stars': merged_df.apply(lambda x: get_stars(x['Model'], x['Vocoder']), axis=1),
     'Model': merged_df['Model'],
     'Vocoder': merged_df['Vocoder'],
     'Duration (device=cpu)': merged_df['Duration_cpu'],
